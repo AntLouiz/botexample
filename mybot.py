671ab25d852c0b6d8536500ae851e3b97b8dea19
Rodrigo <luizrodrigo46@hotmail.com>
2017-02-22 09:05:54 -0300

    fix the message bug

--- mybot.py
+++ mybot.py
@@ -179,14 +179,19 @@ def main():
 		if last_update:
 			if not last_update_id:
 				last_update_id = last_update['update_id']
+				chat_id = last_update['message']['chat']['id']
+			try:
+				message = last_update['message']['text']
+				
 
-			message = last_update['message']['text']
-			chat_id = last_update['message']['chat']['id']
+				if message.lower() in bot.commands:
+					bot.make_command(message, chat_id, last_update_id)
+					last_update_id += 1
+				else:
+					last_update_id += 1
 
-			if message.lower() in bot.commands:
-				bot.make_command(message, chat_id, last_update_id)
-				last_update_id += 1
-			else:
+			except KeyError:
+				bot.send_message("Please just insert messages. :D", chat_id)
 				last_update_id += 1
 
 if __name__ == '__main__':
